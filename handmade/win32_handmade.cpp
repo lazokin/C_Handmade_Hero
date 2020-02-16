575343ff5e5d30d011a975bbe822b01713957144
Nik Ambukovski <nik.ambukovski@gmail.com>
2020-02-16 17:33:18 +1100

    opening a win32 window

--- handmade/win32_handmade.cpp
+++ handmade/win32_handmade.cpp
@@ -1,7 +1,74 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_ACTIVATEAPP:
+		{
+			OutputDebugString(L"WM_ACTIVATEAPP\n");
+		} return 0;
+	case WM_CLOSE:
+		{
+			OutputDebugString(L"WM_CLOSE\n");
+		} return 0;
+	case WM_DESTROY:
+		{
+			OutputDebugString(L"WM_DESTROY\n");
+		} return 0;
+
+	case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hwnd, &ps);
+			PatBlt(hdc, ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right - ps.rcPaint.left, ps.rcPaint.bottom - ps.rcPaint.top, BLACKNESS);
+			EndPaint(hwnd, &ps);
+		} return 0;
+	case WM_SIZE:
+		{
+			OutputDebugString(L"WM_SIZE\n");
+		} return 0;
+	}
+
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-    MessageBox(0, L"This is Handmade Hero", L"Handmade Hero", MB_OK|MB_ICONINFORMATION);
+	WNDCLASS wc = {};
+
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = L"Handmade Hero Window Class";
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowEx(
+		0,
+		wc.lpszClassName,
+		L"Handmade Hero",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (hwnd == NULL)
+	{
+		return 0;
+	}
+
+	MSG msg = { };
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
 	return 0;
 }
